4fc761cc27642dac72a5c0b0c9d652ecf9f92280
Pham Huy Hoang <phamhuyhoang150@gmail.com>
2022-11-01 12:46:26 +0700

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { ContenComponent } from './components/conten/conten.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AllProductsComponent } from './components/all-products/all-products.component';
+import { DetailProductComponent } from './components/detail-product/detail-product.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ContenComponent,
+    FooterComponent,
+    AllProductsComponent,
+    DetailProductComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/conten/conten.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-conten',
+  templateUrl: './conten.component.html',
+  styleUrls: ['./conten.component.scss']
+})
+export class ContenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
